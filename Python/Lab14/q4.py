91d32f384713d03caaa4ec5ecce9265714731781
nileshIBnova25 <nilesh1paerp2@gmail.com>
2025-09-04 18:17:39 +0530

    Add files via upload

---
+++ Python/Lab14/q4.py
@@ -0,0 +1,22 @@
+#For Quadratic Equation
+
+
+
+import cmath
+a=6
+b=-17
+c=12
+
+
+discrminent=b**2 - 4*a*c
+
+route1=(-b+cmath.sqrt(a**2-4*a*c)/2*a)
+route2=(-b-cmath.sqrt(a**2-4*a*c)/2*a)
+
+
+
+
+
+print(discrminent)
+print(route1)
+print(route2)
