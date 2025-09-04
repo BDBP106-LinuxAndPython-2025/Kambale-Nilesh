06367ec9390d7f0b86679f53abbf7bd54c2654e4
nileshIBnova25 <nilesh1paerp2@gmail.com>
2025-09-04 18:15:52 +0530

    Add files via upload

---
+++ Python/Lab14/q3.py
@@ -0,0 +1,25 @@
+# Input two integers k,n
+# Output arithmetic operation between k and n
+
+k=1
+n=10
+summ=k+n
+diff=k-n
+product=k*n
+div=k/n
+expo=k**n
+intdiv=k//n
+mod=k%n
+
+
+#print function automatecally :wq
+
+print(summ)
+print(diff)
+print(product)
+print(div)
+print(intdiv)
+print(expo)
+print(mod)
+
+
